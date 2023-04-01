2c05d0d9e493024fafdeb63e6a5018d27b037fe6
deleram <delaramfaetoot@gmail.com>
2023-04-01 16:00:55 +0430

    Scaled the data

--- first-assignment.py
+++ first-assignment.py
@@ -1,5 +1,6 @@
 # importing needed libraries
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 # First step which is to load and dividing data to X and Y
 dfPath = r'data.csv'
@@ -27,3 +28,20 @@ print(y.head())
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,shuffle=True)
 
 #Second step : It's time to preproccess
+# As we checked there is no missing values and no duplicate ones.
+# And all of our data is numerical, and there are no categorical columns that need to be converted to numerical values.
+# So we should just check if scaling is needed.
+
+# As we can see from the head our columns are not from the same range so we should scale the large ones
+# choose desired columns
+cols_to_scale = ['Elevation', 'Aspect', 'Slope','Horizontal_Distance_To_Hydrology',
+                 'Vertical_Distance_To_Hydrology','Horizontal_Distance_To_Roadways',
+                 'Hillshade_9am','Hillshade_Noon','Hillshade_3pm','Horizontal_Distance_To_Fire_Points'
+]
+#define our scaler
+Scaler = StandardScaler()
+# scale our columns
+Scaler.fit(X[cols_to_scale])
+X[cols_to_scale] = Scaler.transform(X[cols_to_scale])
+
+print(X.head())
